57dacfb0b5a9ce31ee9a8eab2ccf5f963a4f04a2
Minar Hossain <95146879+minarhossain@users.noreply.github.com>
2023-12-11 19:43:49 +0000

    object declaraton

--- object.js
+++ object.js
@@ -1,1 +1,14 @@
-console.log("Hello Word!");
+// In real life objects 
+// In real life a car is an object 
+// A car has properties like weight and color and method like start and stop
+// Objects are variable too but objects can contain many values
+
+const honda = {
+    name: "Honda",
+    brand:"Jampanies",
+    model:"Trigger",
+    color:"Red"
+}
+
+// the values are written as name:value pairs (name and values are seperated by a colon)
+
