3cada536412f93efda2258ae7dfa2ef321cce114
Minar Hossain <95146879+minarhossain@users.noreply.github.com>
2023-12-11 19:54:09 +0000

    object method

--- object.js
+++ object.js
@@ -3,6 +3,9 @@
 // A car has properties like weight and color and method like start and stop
 // Objects are variable too but objects can contain many values
 
+
+// Object declaration and Object literal
+
 const honda = {
     name: "Honda",
     brand:"Jampanies",
@@ -12,3 +15,31 @@ const honda = {
 
 // the values are written as name:value pairs (name and values are seperated by a colon)
 
+// always use object with const keyword
+
+// Object properties (the name:value pairs in JavaScript objects are called properties)
+const obj ={
+    property:"value" // both are properties
+}
+
+// Accessing Object Properties in two ways
+
+// ObjectName.PropertyName   honda.name
+// ObjectName["PropertyName"] honda["name"]
+
+
+// JavaScript Objects are container for named values called properties
+
+
+// Object can also have a methods // Methods are stored in properties as function definitions
+
+const person ={
+    firstName : "Minar ",
+    lastName : "Hossain",
+    id:55698,
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+console.log("Output is here => ", person.fullName())
